a90c6a98bb9779fc211d6297e3c8ede301befcb2
shijunLee <lishjun01@hotmail.com>
2021-06-11 12:15:31 +0800

    add pod webhook

--- api/v1alpha1/helmrepo_webhook.go
+++ api/v1alpha1/helmrepo_webhook.go
@@ -18,11 +18,12 @@ package v1alpha1
 
 import (
 	"errors"
+	"strings"
+
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
-	"strings"
 )
 
 // log is for logging in this package.
